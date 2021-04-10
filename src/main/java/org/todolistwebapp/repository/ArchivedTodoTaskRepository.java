01fa0afbfeef7b2ff582d66eb02528bbda5e7627
Christoph Tornau <christoph.tornau@inf.h-brs.de>
2021-04-10 18:24:30 +0200

    Annotationen hinzugefügt

--- src/main/java/org/todolistwebapp/repository/ArchivedTodoTaskRepository.java
+++ src/main/java/org/todolistwebapp/repository/ArchivedTodoTaskRepository.java
@@ -1,7 +1,9 @@
 package org.todolistwebapp.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 import org.todolistwebapp.model.ArchivedTodoTask;
 
+@Repository
 public interface ArchivedTodoTaskRepository extends MongoRepository<ArchivedTodoTask, Long> {
 }
